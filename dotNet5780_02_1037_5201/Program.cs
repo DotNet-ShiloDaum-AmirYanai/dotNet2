f0b6f73881fa45f43be9da7550aff76c27816c39
shilodaum <shilodaum@gmail.com>
2019-11-20 17:21:59 +0200

    add files

---
+++ dotNet5780_02_1037_5201/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_02_1037_5201
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
