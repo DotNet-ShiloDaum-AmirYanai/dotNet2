34b640739f204c0e23eeeb7691e00d0252340db2
shilodaum <shilodaum@gmail.com>
2019-11-20 18:50:09 +0200

    add ctor and overrides

--- dotNet5780_02_1037_5201/HostingUnit.cs
+++ dotNet5780_02_1037_5201/HostingUnit.cs
@@ -2,6 +2,23 @@ namespace dotNet5780_02_1037_5201
 {
     internal class HostingUnit
     {
-        
+        public HostingUnit()
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
